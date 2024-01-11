88e270915d8e52dfd5cf9ec234e225bf14502abc
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2024-01-11 15:16:51 -0300

    Update sketch.ino
    Configuração de Leds e piscar uma sequência inicial de teste

--- Genius/sketch.ino
+++ Genius/sketch.ino
@@ -3,16 +3,34 @@
 #define Verde 4
 #define Amarelo 5
 
-int Leds[4] = {2,3,4,5}
+#define UM_SEG 1000
+#define MEIO_SEG 500
+
+int Leds[4] = {Vermelho, Azul, Verde, Amarelo};
 
 void setup() {
+  //Inicia portas
   for(int i=0; i<4; i++){
     pinMode(Leds[i], OUTPUT);
   }
-
+  //Testa LEDs
+  sequenciaInit();
 }
 
 void loop() {
   
   
+}
+
+void piscaLed(int LED, int tempo){
+  digitalWrite(LED, HIGH);
+  delay(tempo);
+  digitalWrite(LED, LOW);
+  delay(tempo);
+}
+
+void sequenciaInit(){
+  for(int i=0;i<4;i++){
+    piscaLed(Leds[i], MEIO_SEG);
+  }
 }
